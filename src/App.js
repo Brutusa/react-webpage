f663e003d2a58f614db308b157f7a6629bf4b9ff
Andujar <andujar.brutus@gmail.com>
2023-03-08 13:41:37 -0500

    inital react files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This page is a work in progress by Andujar Brutus.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/Brutusa"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Andujar's Github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
